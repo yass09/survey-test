8340f67946d1a88e44b7f81e9fc9a905ce25776a
Yacine Chibane-Devereux <y.chibane@me.com>
2019-12-09 18:07:37 +0100

    setup survey item component

---
+++ src/components/presentational/SurveyItem.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import React from "react";
+
+import NavigationLink from "./NavigationLink";
+import { SurveyItemElement, SurveyItemElementText } from "./SurveyItemElements";
+
+const Item = styled.li`
+  margin: 0.5em 0;
+  width: 100%;
+`;
+
+const SurveyLink = styled(NavigationLink)`
+  align-items: center;
+  border: 0.05em solid black;
+  border-radius: 0.15em;
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5em;
+`;
+
+const SurveyItem = ({ survey }) => {
+  const { name, code } = survey;
+  return (
+    <Item>
+      <SurveyLink to={`/survey/${name}`}>
+        <SurveyItemElement>
+          <SurveyItemElementText>{name}</SurveyItemElementText>
+        </SurveyItemElement>
+        <SurveyItemElement>
+          <SurveyItemElementText>{code}</SurveyItemElementText>
+        </SurveyItemElement>
+      </SurveyLink>
+    </Item>
+  );
+};
+
+export default SurveyItem;
---
+++ src/components/presentational/SurveyItemElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SurveyItemElement = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`;
+export const SurveyItemElementText = styled.p`
+  margin: 0;
+`;
