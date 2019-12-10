0e06a35d951317cf6cc732e6c7ef051562d802c9
Yacine Chibane-Devereux <y.chibane@me.com>
2019-12-10 20:25:34 +0100

    added reformat of data

--- src/components/views/SurveyDetailsView.js
+++ src/components/views/SurveyDetailsView.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { getData } from "../../helpers";
+import { getDetailsUrl } from "../../constants";
+import SurveysContext from "../../context/SurveysContext";
 
 import ViewHeader from "../presentational/ViewHeader";
 import ViewContentWrapper from "../presentational/ViewContentWrapper";
 import ViewSection from "../presentational/ViewSection";
 
-const SurveyDetailsView = () => {
+const SurveyDetailsView = props => {
+  const { state: { selectedSurvey: { code, name } = {} } = {} } = useContext(
+    SurveysContext
+  );
+  const [surveyDetails, setSurveyDetails] = useState([]);
+  const {
+    match: { params: { code: codeParam, name: nameParam } = {} } = {}
+  } = props;
+  const cityCode = code || codeParam;
+  const cityName = name || nameParam;
+
+  const sortDates = (a, b) => new Date(a) - new Date(b);
+
+  const detailsUrl = getDetailsUrl(cityCode);
+  useEffect(() => {
+    const formatData = data => {
+      if (data.type === "qcm") {
+        let arrayObject = [];
+        const object = data.result;
+        for (let prop in object) {
+          const objectItem = {
+            productName: prop,
+            number: object[prop]
+          };
+          arrayObject.push(objectItem);
+        }
+        return {
+          ...data,
+          result: arrayObject
+        };
+      }
+      if (data.type === "numeric") {
+        return {
+          ...data,
+          result: Math.ceil(data.result)
+        };
+      }
+      if (data.type === "date") {
+        return {
+          ...data,
+          result: data.result.sort(sortDates)
+        };
+      }
+    };
+    const getSurveyDetails = async () => {
+      await getData(detailsUrl)
+        .then(response => {
+          const formatedData = response.map(formatData);
+          setSurveyDetails(formatedData);
+        })
+        .catch(e => console.log(e));
+    };
+    getSurveyDetails();
+  }, [detailsUrl]);
+
   return (
     <ViewSection>
-      <ViewHeader title={"Product List"} />
+      <ViewHeader title={cityName} />
       <ViewContentWrapper></ViewContentWrapper>
     </ViewSection>
   );
